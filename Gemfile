b2476492b2eb147e370c7906cb22d44794f59726
Nick Sutterer <apotonick@gmail.com>
2016-01-22 13:26:48 +1100

    bigger form!

--- Gemfile
+++ Gemfile
@@ -3,14 +3,14 @@ source 'https://rubygems.org'
 gem "sinatra"
 gem "sinatra-sequel"            # persistence
 gem "sqlite3"
-gem "trailblazer", "1.1.0.rc1"  # operation
+gem "trailblazer", "1.1.0"  # operation
 # gem "reform",      "2.1.0.rc1"  # validation
 gem "reform",      path: "../reform"  # validation
 gem "dry-validation", ">= 0.4.0"
 gem "cells"                     # presentation
 gem "cells-slim", ">= 0.0.4"
-
-
-gem "trailblazer-cells", path: "../trailblazer-cells"
+gem "trailblazer-cells"
 
 gem "rerun"
+
+gem "formular", path: "../formular"
--- concepts/post/operation/create.rb
+++ concepts/post/operation/create.rb
@@ -5,21 +5,33 @@ class Post::Create < Trailblazer::Operation
     property :title
     property :url_slug
     property :content
+    property :is_public, virtual: true
+    property :owner, virtual: true
+    property :roles, virtual: true
 
     include Reform::Form::Dry::Validations
 
     validation :default do
       key(:title, &:filled?)
       key(:url_slug) { |slug| slug.format?(/^[\w-]+$/) & slug.unique? }
+      key(:content) { |content| content.max_size?(10) } # i know that a real blog post should be a bit more elaborating.
 
       def unique?(value)
         form.model.class[url_slug: value].nil?
       end
 
+      key(:roles) { |roles| roles.size?(2) }
+
       configure { |config|
         config.messages_file = 'concepts/post/operation/dry_error_messages.yml'
       }
     end
+
+    # def is_public
+    #   return 1 if super.nil?
+    #   return 1 if super.to_s=="1"
+    #   0
+    # end
   end
 
   def model!(*)
@@ -27,6 +39,7 @@ class Post::Create < Trailblazer::Operation
   end
 
   def process(params)
+    puts "@@@@@ #{params.inspect}"
     validate(params) do
       contract.save
     end
